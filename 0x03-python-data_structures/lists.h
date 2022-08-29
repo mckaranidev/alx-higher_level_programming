146eccbc867ca87536691210e1b73fe99143bec4
mckaranidev <mckarani.dev@gmail.com>
2022-08-29 16:51:27 +0000

    Adding 13-is_palindrome.c and lists.h

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -1,15 +1,16 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stddef.h>
+
 /**
  * struct listint_s - singly linked list
  * @n: integer
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * for project
+ * for Holberton project
  */
-#include <stddef.h>
 typedef struct listint_s
 {
     int n;
